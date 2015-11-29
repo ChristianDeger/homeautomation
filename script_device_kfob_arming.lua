e91c5551364ce734df1f113f99c2fecc29e34e05
Christian Deger <christian@deger.eu>
2015-11-29 19:45:55 +0100

    Playing with KFOB

---
+++ script_device_kfob_arming.lua
@@ -0,0 +1,12 @@
+commandArray = {}
+
+print('1')
+print(devicechanged['KFOB-C-1'])
+print('3')
+print(devicechanged['KFOB-C-3'])
+
+
+-- if (devicechanged['Armed'] == 'Off') then
+--   commandArray['Intrusion'] = 'Off'
+--   print('**** Armed off -> Intrusion off')
+-- else
