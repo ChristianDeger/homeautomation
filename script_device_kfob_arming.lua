786430bfbb56408139a31b7e43ad0a9402fadb6d
Christian Deger <christian@deger.eu>
2015-11-29 20:15:39 +0100

    Switching lights with KFOB 1

--- script_device_kfob_arming.lua
+++ script_device_kfob_arming.lua
@@ -1,9 +1,9 @@
 commandArray = {}
 
 if (devicechanged['KFOB-C-1'] == 'On') then
-  commandArray['Light Livingroom'] = 'On'
+  commandArray['Group:Light Livingroom'] = 'On'
 elseif (devicechanged['KFOB-C-3'] == 'On') then
-  commandArray['Light Livingroom'] = 'On'
+  commandArray['Group:Light Livingroom'] = 'Off'
 end
 
 return commandArray
