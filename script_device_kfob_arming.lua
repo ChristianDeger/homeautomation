27206c433b7ad9cbb9245cf487b4d5283e43112a
Christian Deger <christian@deger.eu>
2015-11-29 20:17:55 +0100

    Arming with KFOB

--- script_device_kfob_arming.lua
+++ script_device_kfob_arming.lua
@@ -1,9 +1,9 @@
 commandArray = {}
 
 if (devicechanged['KFOB-C-1'] == 'On') then
-  commandArray['Group:Light Livingroom'] = 'On'
+  commandArray['Armed'] = 'On'
 elseif (devicechanged['KFOB-C-3'] == 'On') then
-  commandArray['Group:Light Livingroom'] = 'Off'
+  commandArray['Armed'] = 'Off'
 end
 
 return commandArray
