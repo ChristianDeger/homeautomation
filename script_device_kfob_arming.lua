b849cba5cc1120073415f765d15393f6f4ec014b
Christian Deger <christian@deger.eu>
2015-12-07 19:55:56 +0100

    Arm via kfob with delay

--- script_device_kfob_arming.lua
+++ script_device_kfob_arming.lua
@@ -1,7 +1,9 @@
 commandArray = {}
 
+armingDelay = '30'
+
 if (devicechanged['KFOB-C-1'] == 'On') then
-  commandArray['Armed'] = 'On'
+  commandArray['Armed'] = 'On AFTER '..armingDelay
 elseif (devicechanged['KFOB-C-3'] == 'On') then
   commandArray['Armed'] = 'Off'
 end
