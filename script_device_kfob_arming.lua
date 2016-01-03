077599c959a11c3c34b01e37610325d426b30e75
Christian Deger <christian@deger.eu>
2016-01-03 19:22:35 +0100

    Also switch intrusion off, when disarming via KFOB

--- script_device_kfob_arming.lua
+++ script_device_kfob_arming.lua
@@ -6,6 +6,7 @@ if (devicechanged['KFOB-C-1'] == 'On') then
   commandArray['Armed'] = 'On AFTER '..armingDelay
 elseif (devicechanged['KFOB-C-3'] == 'On') then
   commandArray['Armed'] = 'Off'
+  commandArray['Intrusion'] = 'Off'
 end
 
 return commandArray
